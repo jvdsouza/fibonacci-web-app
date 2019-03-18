cad750ad0a838cab6dcfeed736364f0c7fdf9466
Jason D'souza <jvincentdsouza@gmail.com>
2019-03-18 21:20:15 +1100

    initial commit with minimum layout done, and a button that does nothing

--- src/App.js
+++ src/App.js
@@ -3,23 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+      fibVal: ''
+    }
+  }
+
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onSubmit = () => {
+    fetch('http://localhost:3001/fibonacci', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="main Helvetica tc">
+        <div className="ma2">Which number of the fibonacci sequence do you want?</div>
+        <input className="ma2" type="text" onChange={this.onInputChange}/>
+        <button className="button" onClick={this.onSubmit}>Search</button>
+        <div>
+          The number {this.state.input} represents:
+          <div>
+            <h1></h1>
+          </div>
+        </div>
       </div>
     );
   }
